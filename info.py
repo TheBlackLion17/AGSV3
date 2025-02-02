f94986e07cfe5abea7516aa5fda8a43ff8e15e39
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-02-02 21:56:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # PyroClient Setup 
 API_ID = int(environ.get('API_ID','20919286'))
 API_HASH = environ.get('API_HASH','57b85f72104db3f08f9795b0410eb556')
-BOT_TOKEN = environ.get('BOT_TOKEN', '5800701341:AAHuWVDFDDT3jk-p0H0yOjM2ZyHc0KiZOHc')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6146949032:AAFAVhQe8ZrfqLJGoagO0zxnJs5M2dZnzuY')
 
 
 # Bot settings
